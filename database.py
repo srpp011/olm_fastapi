a78a91b387e45651ee4626563184d07da2f16b91
srpp011 <srpp011@gmail.com>
2024-02-22 15:29:44 +0100

    stats db table created

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-#URL_DATABSE = 'postgresql://gilab:openlandmap@localhost:5436/data'
+# URL_DATABSE = 'postgresql://gilab:openlandmap@localhost:5436/data'
 URL_DATABSE = 'postgresql://fastapi:openlandmap@stats_db:5432/stats'
 
 engine = create_engine(URL_DATABSE)
